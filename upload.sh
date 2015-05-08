aea4257f13b2bdffb91c91a2fbba6804b475ec18
Stephen Parente <webmakersteve@gmail.com>
2015-05-08 12:01:25 -0400

    Updated CSS

--- upload.sh
+++
@@ -1,6 +0,0 @@
-jekyll build
-
-aws s3 cp ./_site/css/main.css s3://inc-broll/TOP/css/main.css --acl public-read
-aws s3 cp ./_site/js/main.js s3://inc-broll/TOP/js/main.js --acl public-read
-
-cat ./_site/partial/index.html | pbcopy
